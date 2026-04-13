79383f80b024e25a98598b0688ff3f81ff3c8c71
MSYDJT <msydjt365@gmail.com>
2026-04-13 12:33:09 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,27 +9,22 @@ import type {
 } from "./types/config";
 import { LinkPreset } from "./types/config";
 
-const customDomain = "2x.nz";
+const customDomain = "blog.msydjt.com";
 
 export const siteConfig: SiteConfig = {
 	customDomain,
-	title: "《二叉树树》官方网站",
-	subtitle: "AcoFork",
+	title: "MSYDJT'Blog",
+	subtitle: "门上一点就通",
 	description:
-		"《二叉树树》是一个专注于IT/互联网技术分享与实践的个人技术博客，在这里你可以找到众多前沿技术的分享与实践经验。",
+		"一个专注于IT/互联网技术分享与实践的个人技术博客，在这里你可以找到众多前沿技术的分享与实践经验。",
 
 	keywords: [
-		"二叉树树",
-		"二叉树树官网",
-		"树",
-		"二叉树",
-		"二叉",
+		"MSYDJT",
+		"门上一点就通官网",
+		"门上一点就通",
+		"MSYDJT官网",
 		"博客",
-		"AcoFork Blog",
-		"AcoFork",
 		"Blog",
-		"acofork blog",
-		"acofork",
 		"blog",
 	],
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
